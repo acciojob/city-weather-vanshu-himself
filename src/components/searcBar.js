0c62b042fa0b9e18956274f9a02e4f00710fdf01
Vansh Gupta <you@example.com>
2023-07-25 20:07:47 +0530

    3rd

--- src/components/searcBar.js
+++ src/components/searcBar.js
@@ -39,14 +39,15 @@ const API_KEY="07942d0de47eb767714734c476425578";
           <div>
 
     {
-        weathers &&  <div className="weather"> {
-            <>
+        weathers &&   
+            <div className="weather">
+           
             <h2>{weathers.name}</h2>
            <h1>{weathers.main.temp}F</h1>
            <p>{weathers.weather[0].description}</p>
             
-           </>
-        }</div>
+           </div>
+        
     }
           </div>
         </div>
