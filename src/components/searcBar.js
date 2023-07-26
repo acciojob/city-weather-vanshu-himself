99808d970efd3b4ab3bbf8bfb7be87fcc419fb79
Vansh Gupta <you@example.com>
2023-07-26 12:26:09 +0530

    4rth

--- src/components/searcBar.js
+++ src/components/searcBar.js
@@ -39,14 +39,20 @@ const API_KEY="07942d0de47eb767714734c476425578";
           <div>
 
     {
-        weathers &&   
+        weathers &&   (
             <div className="weather">
            
             <h2>{weathers.name}</h2>
-           <h1>{weathers.main.temp}F</h1>
+           <h1>{weathers.main.temp}°F</h1>
            <p>{weathers.weather[0].description}</p>
+           <div className="icon">
+            <img
+              src={`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`}
+              alt={weather.weather[0].description}
+            />
+          </div>
             
-           </div>
+           </div>)
         
     }
           </div>
